d184d6cc795ab1728ba11902b94a713f8f6a0c87
archds <democlaus202@gmail.com>
2021-09-23 18:19:49 +0700

    alter script

--- server/filehost/SendMSInfo.ps1
+++ server/filehost/SendMSInfo.ps1
@@ -1,5 +1,8 @@
+$ip = $args[0]
+$port = $args[1]
+$Uri = "http://${ip}:${port}/api/collect-msinfo/"
 $Guid = New-Guid
 $FileName = $Guid.toString() + '.nfo'
-$FilePath = 'C:\Users\User\' + $FileName
+$FilePath = "C:\Users\User\${FileName}"
 msinfo32.exe /nfo $FilePath | Out-Null
-Invoke-RestMethod -Uri 'http://localhost:8001/api/collect-msinfo/' -Method Post -InFile $FilePath -UseDefaultCredentials -AllowUnencryptedAuthentication
+Invoke-RestMethod -Uri $Uri -Method Post -InFile $FilePath -UseDefaultCredentials -AllowUnencryptedAuthentication
