513620ade8d008c8495f18e30fce1e3b0d72f3ed
archds <dsalekseev11@gmail.com>
2021-11-27 12:57:12 +0700

    fix import submodules

--- server/gql_api/schema.py
+++ server/gql_api/schema.py
@@ -1,7 +1,7 @@
 import importlib
 import pkgutil
 from types import ModuleType
-from typing import Dict
+from typing import Union
 
 from ariadne import make_executable_schema
 
@@ -9,31 +9,22 @@ import gql_api.resolvers as root_resolvers
 from gql_api.type_defs import resolvers, type_defs
 
 
-def import_submodules(package, recursive: bool = True) -> Dict[str, ModuleType]:
+def import_submodules(package: Union[ModuleType, str], recursive: bool = True) -> None:
     """
     Рекурсивно импортирует все модули в папке.
-
-    :param package:
-    :param recursive:
-    :return:
     """
 
     if isinstance(package, str):
         package = importlib.import_module(package)
     elif not isinstance(package, ModuleType):
-        raise TypeError('must provide a module path or an inported module')
+        raise TypeError('must provide a module path or an imported module')
 
-    results = {}
-
-    for loader, name, is_pkg in pkgutil.walk_packages(package.__path__):
-        full_name = package.__name__ + '.' + name
-        results[full_name] = importlib.import_module(full_name)
+    for loader, name, is_pkg in pkgutil.walk_packages(package.__path__, package.__name__ + '.'):
+        importlib.import_module(name)
         if recursive and is_pkg:
-            results.update(import_submodules(full_name))
-
-    return results
+            import_submodules(package=name)
 
 
-import_submodules(root_resolvers)
+import_submodules(root_resolvers)  # noqa
 
 schema = make_executable_schema(type_defs, resolvers)
