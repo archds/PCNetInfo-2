ace8b50afc5459f00c61744f1fa6ee43ede582fe
danil <Ockolok.3la@gmail.com>
2021-12-01 22:15:09 +0700

    nav

--- webapp/components/navigation.tsx
+++ webapp/components/navigation.tsx
@@ -1,21 +1,21 @@
 import { Paper, Tab, Tabs } from '@material-ui/core'
 import { useRouter } from 'next/router'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Navigation() {
     const router = useRouter()
-    const [value, setValue] = useState('/')
+    const [value, setValue] = useState(router.pathname)
     const handleClick = (path) => {
         setValue(path)
         router.push(path)
     }
-
     return (
         <header>
             <Paper>
                 <Tabs centered value={value} textColor='primary' indicatorColor='primary'>
                     <Tab value='/' label='Computers' onClick={() => handleClick('/')}/>
                     <Tab value='/monitors' label='Monitors' onClick={() => handleClick('/monitors')}/>
+                    <Tab value='/locations' label='Locations'  onClick={() => handleClick('/locations')}/>
                 </Tabs>
             </Paper>
         </header>
