0c5943c7c6188c54fc583de5168d9a8d2a9fcdf4
archds <dsalekseev11@gmail.com>
2021-12-02 23:07:40 +0700

    tests

--- server/tests/graphql/mutations.py
+++ server/tests/graphql/mutations.py
@@ -103,3 +103,20 @@ def send_delete_location_mutation(id: str):
         name='deleteLocation',
         variables={'id': id}
     )
+
+
+def send_auth_mutation(username: str, password: str):
+    return send_mutation(
+        name='auth',
+        variables={
+            'username': username,
+            'password': password
+        }
+    )
+
+
+def send_verify_token_mutation(token: str):
+    return send_mutation(
+        name='verifyToken',
+        variables={'token': token}
+    )
