00d4538a31a0853306ff7af18ea7a4fe7fc18a8f
archds <dsalekseev11@gmail.com>
2021-12-06 00:58:35 +0700

    setup apollo

--- webapp/apollo-client.ts
+++ webapp/apollo-client.ts
@@ -1,7 +1,25 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { getCookie } from 'cookies-next'
+
+const httpLink = createHttpLink({
+    uri: `${process.env.NEXT_PUBLIC_BACKEND_ROOT_URI}/api/`,
+})
+
+
+const authLink = setContext((_, { headers }) => {
+    const token = String(getCookie('authToken'))
+
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Token ${token}` : '',
+        },
+    }
+})
 
 const client = new ApolloClient({
-    uri: `${process.env.NEXT_PUBLIC_BACKEND_ROOT_URI}/api/`,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
 })
 
--- webapp/components/AuthProvider.tsx
+++ webapp/components/AuthProvider.tsx
@@ -6,15 +6,20 @@ import style from 'pages/auth.module.scss'
 import React, { useRef, useState } from 'react'
 
 function AuthProvider(props) {
-    const [authLoading, setAuthLoading] = useState(false)
-    const [auth, { error: authError }] = useAuthLazyQuery({
-        onCompleted: data => {
-            setCookies('authToken', data.auth.token, { path: '/', maxAge: 259200 })
-            setAuthLoading(true)
+    const [authLoading, setAuthLoading] = useState<boolean>(false)
+    const [authError, setAuthError] = useState<boolean>(false)
+    const [auth] = useAuthLazyQuery({
+        onCompleted: result => {
+            if (result.auth.valid) {
+                setCookies('authToken', result.auth.token, { path: '/', maxAge: 259200 })
+                setAuthLoading(true)
+            } else {
+                setAuthError(true)
+            }
         },
     })
 
-    const { loading: verifyLoading, data: verifyData } = useVerifyTokenQuery({
+    const { loading: verifyLoading, data: verifyResult } = useVerifyTokenQuery({
         variables: {
             token: String(getCookie('authToken')),
         },
@@ -33,7 +38,7 @@ function AuthProvider(props) {
     }
 
     if (verifyLoading) return <LinearProgress/>
-    if (verifyData) return props.children
+    if (verifyResult.verifyToken.valid) return props.children
 
     return (
         <>
@@ -42,8 +47,8 @@ function AuthProvider(props) {
                     <Typography variant='h3' fontSize={26}>
                         Login
                     </Typography>
-                    <TextField id='username' label='Username' type='text' ref={usernameEl} error={!!authError}/>
-                    <TextField id='password' label='Password' type='password' ref={passwordEl} error={!!authError}/>
+                    <TextField id='username' label='Username' type='text' ref={usernameEl} error={authError}/>
+                    <TextField id='password' label='Password' type='password' ref={passwordEl} error={authError}/>
                     <LoadingButton
                         loading={authLoading || verifyLoading}
                         variant='contained'
