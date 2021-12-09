b825370fa0133c9532409dc713859619c43a00d9
archds <democlaus202@gmail.com>
2021-12-09 17:35:19 +0700

    workflow test

--- webapp/pages/_app.tsx
+++ webapp/pages/_app.tsx
@@ -28,7 +28,11 @@ declare module '@mui/styles/defaultTheme' {
 }
 
 const generateClassName = createGenerateClassName({
+    // By enabling this option, if you have non-MUI elements (e.g. `<div />`)
+    // using MUI classes (e.g. `.MuiButton`) they will lose styles.
+    // Make sure to convert them to use `styled()` or `<Box />` first.
     disableGlobal: true,
+    // Class names will receive this seed to avoid name collisions.
     seed: 'mui-jss',
 })
 
